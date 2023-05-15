0bf216b95083de237ad34d10492e84360de937e3
Evandenilo Neto <132853524+Evndrz2000@users.noreply.github.com>
2023-05-15 20:06:39 -0300

    Add files via upload

---
+++ Semana 12/T1/Sem-12-T1-Q1 Corrida tartaruga e a lebre.py
@@ -0,0 +1,7 @@
+Dtartaruga = int(input("Digite a distância que a tartaruga saiu à frente da lebre: "))
+
+Vtartaruga = 1
+Vlebre = 10
+tempo = Dtartaruga / (Vlebre - Vtartaruga)
+
+print(f"{tempo :.0f}")
