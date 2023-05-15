3640bfd1a42cd0e69d9edb8ba5829246b981c095
Evandenilo Neto <132853524+Evndrz2000@users.noreply.github.com>
2023-05-15 09:59:37 -0300

    Delete Semana 12/T2 directory

--- Semana 12/T2/Sem-12-T2-Q3 Calcular H = 1 + 12 + 13 + 14 + ... + 1n.py
+++
@@ -1,8 +0,0 @@
-def calcular_H(n):
-    H = 1
-    for i in range(2, n+1):
-        H += 1/i
-    return H
-num = int(input("Digite o valor de n: ").strip())
-resultado = calcular_H(num)
-print(f"o valor de H é: {resultado:.4f}")
