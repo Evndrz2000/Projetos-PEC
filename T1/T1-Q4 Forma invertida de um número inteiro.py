ddd6eed0f047cd993781681fea13ce9b1b1be713
Evandenilo Neto <132853524+Evndrz2000@users.noreply.github.com>
2023-06-28 11:02:16 -0300

    Delete T1 directory

--- T1/T1-Q4 Forma invertida de um número inteiro.py
+++
@@ -1,7 +0,0 @@
-def main():
-    numero = int(input("Insira um número inteiro: "))
-    numero_invert = str(numero)[::-1]
-    print(f"O número invertido é {numero_invert}")
-
-if __name__ == '__main__':
-    main()
